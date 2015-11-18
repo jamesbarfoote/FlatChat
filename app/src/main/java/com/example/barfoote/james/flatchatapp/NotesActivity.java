b5ef6b149e585a96f03167d3dd32592c215f5670
James Barfoote <james.barfoote@gmail.com>
2015-11-18 21:48:21 +1300

    started creating the notes activity

--- app/src/main/java/com/example/barfoote/james/flatchatapp/NotesActivity.java
+++ app/src/main/java/com/example/barfoote/james/flatchatapp/NotesActivity.java
@@ -1,17 +1,38 @@
 package com.example.barfoote.james.flatchatapp;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.util.Log;
-        import android.widget.CalendarView;
-        import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jam on 8/06/2015.
  */
-public class NotesActivity extends Activity{
+public class NotesActivity extends ListActivity {
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter=0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Clicked : "+clickCounter++);
+        adapter.notifyDataSetChanged();
     }
 }
