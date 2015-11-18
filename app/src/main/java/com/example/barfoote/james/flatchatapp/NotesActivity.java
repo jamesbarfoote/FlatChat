68e85bd9627121190f4796f6c45f6cf3fb30bb61
James Barfoote <james.barfoote@gmail.com>
2015-11-18 22:05:23 +1300

    basic layout done

--- app/src/main/java/com/example/barfoote/james/flatchatapp/NotesActivity.java
+++ app/src/main/java/com/example/barfoote/james/flatchatapp/NotesActivity.java
@@ -2,8 +2,12 @@ package com.example.barfoote.james.flatchatapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,8 @@ public class NotesActivity extends ListActivity {
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     ArrayList<String> listItems=new ArrayList<String>();
 
+    EditText editText;
+
     //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
     ArrayAdapter<String> adapter;
 
@@ -23,16 +29,32 @@ public class NotesActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        editText = (EditText) findViewById(R.id.notesEditText);
 
         adapter=new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
                 listItems);
         setListAdapter(adapter);
+
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    addItems(v);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
     //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
     public void addItems(View v) {
-        listItems.add("Clicked : "+clickCounter++);
+        String text = editText.getText().toString();
+        editText.setText("");
+        listItems.add(text);
         adapter.notifyDataSetChanged();
     }
 }
