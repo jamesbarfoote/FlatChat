1a4e02a5035c1e5dc412e8df6c249b8f8d932156
James Barfoote <james.barfoote@gmail.com>
2016-02-12 12:10:26 +1300

    finished update php
    needs to be tested yet

--- groupUpdate.php
+++ groupUpdate.php
@@ -13,7 +13,7 @@ define('DB','u997523806_db');
    $OWNER_ID = $_POST['OWNER_ID'];
 
    #$sql = "UPDATE Users ". "SET FlatGroup_name = 'adminG' ". "WHERE Email = 'jimmy2174@gmail.com'" ;
-   $sql = "UPDATE FlatGroups ". "SET FlatGroup_name = $name ". "WHERE GROUP_NAME = $GROUP_NAME" ;
+   $sql = "UPDATE FlatGroups ". "SET SHOPPINGLIST = $SHOPPINGLIST,  CALENDAR = $CALENDAR, MONEY = $MONEY, TODO = $TODO, OWNER_ID = $OWNER_ID". "WHERE GROUP_NAME = $GROUP_NAME" ;
   if(mysqli_query($con,$sql)){
     echo 'successAGU';
   }
